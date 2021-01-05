d8901ba479617557bc39e9f9bc6ddb95c429c070
skalyanpur <sunil.kalyanpur@viasat.com>
2021-01-04 22:45:59 -0800

    palindrome-linked-list

--- tags/linkedlist/palindrome-linked-list.py
+++ tags/linkedlist/palindrome-linked-list.py
@@ -20,3 +20,25 @@ class Solution:
             head = head.next
 
         return l == l[::-1]
+
+    @staticmethod
+    def is_palindrome_smart(head: ListNode) -> bool:
+        """
+
+        :param head:
+        :return:
+        """
+        rev = None
+        slow, fast = head, head
+        while head and head.next:
+            fast = fast.next.next
+            rev, rev.next, slow = slow, rev, slow.next
+
+        if fast:
+            slow = slow.next
+
+        while rev and rev.val == slow.val:
+            slow = slow.next
+            rev = rev.next
+
+        return not rev
